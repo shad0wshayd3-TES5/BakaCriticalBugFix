d00c95c078c0c491913ade460cee0442f67ac8b9
shad0wshayd3 <2724172+shad0wshayd3@users.noreply.github.com>
2026-08-22 18:15:57 -0600

    feat: support 1.7.99

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,47 @@
-namespace ObScript
+namespace Hooks
 {
-	namespace QuitGame
+	class hkQuitGame
 	{
-		inline static constexpr auto LONG_NAME{ "QuitGame"sv };
-
-		static bool IsInModule(std::uintptr_t a_ptr)
+	private:
+		static bool QuitGame()
 		{
-			auto& mod = REL::Module::get();
-			auto seg = mod.segment(REL::Segment::textx);
-			auto end = seg.address() + seg.size();
-			return (seg.address() < a_ptr) && (a_ptr < end);
-		}
+			if (auto log = RE::ConsoleLog::GetSingleton())
+				log->Print("Bye.");
 
-		static bool Execute(const RE::SCRIPT_PARAMETER*, RE::SCRIPT_FUNCTION::ScriptData*, RE::TESObjectREFR*, RE::TESObjectREFR*, RE::Script*, RE::ScriptLocals*, double&, std::uint32_t&)
-		{
-			std::thread{
-				[]
+			std::thread(
+				[]()
 				{
-					RE::ConsoleLog::GetSingleton()->Print("Bye.");
 					std::this_thread::sleep_for(std::chrono::milliseconds(100));
-					RE::Main::GetSingleton()->quitGame = true;
-				}
-			}.detach();
+					if (auto main = RE::Main::GetSingleton())
+						main->quitGame = true;
+				})
+				.detach();
 
 			return true;
 		}
 
+	public:
 		static void Install()
 		{
-			if (auto function = RE::SCRIPT_FUNCTION::LocateConsoleCommand(LONG_NAME))
-			{
-				if (IsInModule(reinterpret_cast<std::uintptr_t>(function->executeFunction)))
-				{
-					function->executeFunction = Execute;
-					SKSE::log::info("Registered function {:s}"sv, LONG_NAME);
-				}
-				else
-				{
-					SKSE::log::warn("{:s} function is already overridden!"sv, LONG_NAME);
-				}
-			}
-			else
-			{
-				SKSE::log::error("Failed to locate function {:s}."sv, LONG_NAME);
-			}
+			static REL::Relocation target{ REL::ID(22896) };
+			target.replace_func(0x28, QuitGame);
 		}
-	}
+	};
+
+	class hkShutdown
+	{
+	private:
+		static void Shutdown()
+		{
+			REX::W32::TerminateProcess(REX::W32::GetCurrentProcess(), EXIT_SUCCESS);
+		}
+
+		inline static REL::THook _Shutdown{ REL::ID(36544), 0x1AE, Shutdown };
+	};
 
 	static void Install()
 	{
-		QuitGame::Install();
+		hkQuitGame::Install();
 	}
 }
 
@@ -60,20 +52,17 @@ namespace
 		switch (a_msg->type)
 		{
 		case SKSE::MessagingInterface::kPostLoad:
-		{
-			ObScript::Install();
+			Hooks::Install();
 			break;
-		}
 		default:
 			break;
 		}
 	}
 }
 
-SKSEPluginLoad(const SKSE::LoadInterface* a_skse)
+SKSE_PLUGIN_LOAD(const SKSE::LoadInterface* a_skse)
 {
-	SKSE::Init(a_skse);
+	SKSE::Init(a_skse, { .trampoline = true });
 	SKSE::GetMessagingInterface()->RegisterListener(MessageCallback);
-
 	return true;
 }
